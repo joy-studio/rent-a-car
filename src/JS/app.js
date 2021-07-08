51fcaab60075637daaecbb8838c45955f206d3f8
Anton <pro.grishin@gmail.com>
2021-07-08 15:06:07 +0300

    first commit

--- src/JS/app.js
+++ src/JS/app.js
@@ -3,33 +3,9 @@ import "../../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import "../CSS/style.scss";
 import "./app.js";
 
-import "../Assets/Images/js_logo.svg";
-import "../Assets/Images/title_background_left.svg";
-import "../Assets/Images/title_background_right.svg";
-import "../Assets/Images/phone_mockup.svg";
-import "../Assets/Images/google_logo.svg";
-import "../Assets/Images/azure_logo.svg";
-import "../Assets/Images/spaceapp_logo.svg";
-import "../Assets/Images/mailru_logo.svg";
-import "../Assets/Images/low-price.svg";
-import "../Assets/Images/free.svg";
-import "../Assets/Images/experience.svg";
-import "../Assets/Images/money.svg";
-import "../Assets/Images/laughing.svg";
-import "../Assets/Images/management.svg";
-import "../Assets/Images/game-controller.svg";
-import "../Assets/Images/internet.svg";
-import "../Assets/Images/mobile-phone.svg";
-import "../Assets/Images/coding.svg";
-
-import "../Assets/Images/about_icon.png";
-import "../Assets/Images/calc.png";
-import "../Assets/Images/games.png";
-import "../Assets/Images/gp_icon.png";
-import "../Assets/Images/apple_icon.png";
-import "../Assets/Images/mobile1.png";
-import "../Assets/Images/mobile2.png";
-
-document.getElementById("ww").textContent = "Width = " + window.innerWidth;
-
-document.getElementById("dialog").shadowRoot();
+import "../Assets/Images/logo.png";
+import "../Assets/Images/phone_icon.png";
+import "../Assets/Images/header_background.png";
+import "../Assets/Images/car_icon.png";
+import "../Assets/Images/price_icon.png";
+import "../Assets/Images/time_icon.png";
